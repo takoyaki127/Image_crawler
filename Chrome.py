543120bc3f297e828e102620bfd0d3b0ad897a19
takoyaki127 <s826062@gmail.com>
2023-04-26 19:02:17 +0900

    initial commit

---
+++ Chrome.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class Chrome():
+    # ブラウザの設定
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--log-level=3')
+
+    def __init__(self):
+        self.driver = webdriver.Chrome(
+            '../chromedriver_win32/chromedriver.exe', options=Chrome.options)
+        self.driver.implicitly_wait(10)
+
+    def close(self):
+        self.driver.close()
+
+    def search_img(self, url, min_width=50, min_height=50, size_lower_limit=True):
+        width = min_width
+        height = min_height
+
+        self.driver.get(url)
+        img_tags = self.driver.find_elements(By.XPATH, "//img")
+
+        img_urls = []
+        for img_tag in img_tags:
+            img_url = self.__excluded_small_image(
+                img_tag, width, height, size_lower_limit)
+            if img_url:
+                img_urls.append(img_url)
+
+        return img_urls
+
+    def __excluded_small_image(self, img_tag: WebElement, width, height, size_lower_limit):
+        if size_lower_limit == False:
+            return img_tag.get_attribute('src')
+        if self.__isLarge(img_tag, 'width', width):
+            if self.__isLarge(img_tag, 'height', height):
+                return img_tag.get_attribute('src')
+        return None
+
+    def __isLarge(self, tag: WebElement, side, size):
+        return int(tag.get_attribute(side)) >= size
---
+++ Crawling.py
@@ -0,0 +1,53 @@
+import os
+import requests
+import datetime
+import time
+from tqdm.contrib import tenumerate
+
+from Chrome import Chrome
+
+
+def now():
+    dt_now = datetime.datetime.now()
+    current_time = dt_now.strftime(f"%Y_%m%d_%H%M%S")
+    return current_time
+
+
+def read_file():
+    try:
+        with open('./settings.txt', mode='r') as f:
+            dir = f.read().split('=')[1]
+            return dir
+    except:
+        print('settings.txtファイルが見つかりません')
+        exit()
+
+
+def create_directly(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+
+def download_image(url, file_path):
+    r = requests.get(url, stream=True)
+
+    if r.status_code == 200:
+        with open(file_path, "wb") as f:
+            f.write(r.content)
+
+
+file = read_file()
+browser = Chrome()
+page_url = input('URL->')
+
+img_urls = browser.search_img(page_url, size_lower_limit=False)
+if img_urls:
+    current_time = now()
+    folder_name = f'{file}/{current_time}'
+    create_directly(folder_name)
+
+    for i, img_url in tenumerate(img_urls):
+        download_image(img_url, f'{folder_name}/{i}.png')
+        time.sleep(1)
+
+browser.close()
